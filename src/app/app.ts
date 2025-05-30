befbb89611a36438edf2b560110328f8bab8c2c1
TehilaTheStudent <tehila14916@gmail.com>
2025-05-30 17:00:51 +0300

    a

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Emojis } from "./emojis/emojis";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Emojis],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
